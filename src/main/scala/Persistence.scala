8b6933d9b46ae91751c707e74a5f4ad65e570056
Kuat Yessenov <kuat@csail.mit.edu>
2011-05-26 15:29:26 -0400

    Fix some naming issues

--- src/main/scala/Persistence.scala
+++ src/main/scala/Persistence.scala
@@ -5,15 +5,15 @@ import java.io.ByteArrayOutputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
-object SceevesStr {
-  def serialize[T] (e : T) : Array[Byte] = {
+object Persistence {
+  def serialize(e : AnyRef) : Array[Byte] = {
     val baos = new ByteArrayOutputStream(1024);
     val o = new ObjectOutputStream(baos);
     o.writeObject(e);
     baos.toByteArray();
   }
 
-  def unserialize[T] (s : Array[Byte]) : T = {
+  def deserialize[T] (s : Array[Byte]) : T = {
     val bais = new ByteArrayInputStream(s);
     val input = new ObjectInputStream(bais);
     val obj = input.readObject();
