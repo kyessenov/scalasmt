f88789c125ee1806d93a55a507e0f1b08ae24782
Jean Yang <jean.yang.writeme@gmail.com>
2011-05-26 11:46:16 -0400

    added a test for symbolic contexts

--- src/main/scala/DatabaseLib.scala
+++ src/main/scala/DatabaseLib.scala
@@ -2,5 +2,10 @@ package cap.scalasmt
 
 
 object DatabaseLib {
+  /* Setting this up. */
+  def connectToDatabase() = throw Undefined
+  def makeTable() = throw Undefined
+
+  def putField[T] (field : String, item : T) : Unit = throw Undefined
   def getField[T] (field : String) : Option[T] = throw Undefined
 }
