9b56ca14a80b630abab17923a4f7f15a3a1463bf
Kuat Yessenov <kuat@csail.mit.edu>
2011-05-27 17:56:48 -0400

    Fix implicit for the tests

---
+++ src/main/scala/Persistence.scala
@@ -0,0 +1,23 @@
+package cap.scalasmt
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+object Persistence {
+  def serialize(e : AnyRef) : Array[Byte] = {
+    val baos = new ByteArrayOutputStream(1024);
+    val o = new ObjectOutputStream(baos);
+    o.writeObject(e);
+    baos.toByteArray();
+  }
+
+  def deserialize[T] (s : Array[Byte]) : T = {
+    val bais = new ByteArrayInputStream(s);
+    val input = new ObjectInputStream(bais);
+    val obj = input.readObject();
+    obj.asInstanceOf[T]
+  }
+}
+
