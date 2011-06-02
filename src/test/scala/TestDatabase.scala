83d6044e3dbb6e94628f5f3f29448767b729801e
Jean Yang <jean.yang.writeme@gmail.com>
2011-06-02 15:16:47 -0400

    updated tests

--- src/test/scala/TestDatabase.scala
+++ src/test/scala/TestDatabase.scala
@@ -9,7 +9,7 @@ import java.sql
 import JeevesLib._
 
 class ExampleDatabase extends FunSuite {
-  private val context = pick(_ => true);
+  private val context = pickAtom;
   private val uRecord =
     new UserRecord( 0
                   , Constant(0), 0  // name, namep
@@ -18,7 +18,7 @@ class ExampleDatabase extends FunSuite {
                   , Constant(0), 0  // email, emailp
                   , Constant(0), 0  // network, networkp
                   , List(), 0  // friends, friendsp
-                  , context, List(0))
+                  , context )
 
   private def mkTestDB() : Database = {
     val db = new Database();
@@ -37,5 +37,21 @@ class ExampleDatabase extends FunSuite {
     val idx = pick(x => x === uRecord.getUname());
     val entry = db.getEntry(idx);
     expect(uRecord) { concretize(entry) }
+  }
+
+  test ("symbolic record field") {
+    val db = mkTestDB();
+    val idx = pick(x => x === uRecord.getUname());
+    val entry = db.getEntry(idx);
+    val x = pick(x => (entry ~ '__pwd === 0) ==> (x === 42));
+    expect(42) { concretize(context, uRecord, x) }
+  }
+
+  test ("symbolic record getter") {
+    val db = mkTestDB();
+    val idx = pick(x => x === uRecord.getUname());
+    val entry = db.getEntry(idx);
+    val x = pick(x => (entry ~ 'getPwd === 0) ==> (x === 42));
+    expect(42) { concretize(context, uRecord, x) }
   }
 }
--- src/test/scala/TestUserRecord.scala
+++ src/test/scala/TestUserRecord.scala
@@ -8,8 +8,9 @@ import java.sql
 import JeevesLib._
 
 class ExampleUserRecord extends FunSuite {
-  test ("equalsSelf") {
-    val context = pick (_ => true);
+  val context = pickAtom;
+
+  test ("equals self") {
     val u =
       new UserRecord(0
                   , Constant(0), 0  // name, namep
@@ -18,8 +19,81 @@ class ExampleUserRecord extends FunSuite {
                   , Constant(0), 0  // email, emailp
                   , Constant(0), 0  // network, networkp
                   , List(), 0  // friends, friendsp
-                  , context, List(0));
+                  , context );
     expect(true) { u.equals(u)  };
   }
-  // TODO: More substantive tests.
+
+  test ("isFriends") {
+    val u =
+      new UserRecord(0
+                  , Constant(0), 0  // name, namep
+                  , Constant(0), 0  // pwd, pwdp
+                  , Constant(0), 0  // username, usernamep
+                  , Constant(0), 0  // email, emailp
+                  , Constant(0), 0  // network, networkp
+                  , List(), 0  // friends, friendsp
+                  , context );
+    u.addFriend(1);
+    expect(true) { u.isFriends(1) };
+  }
+
+  // NOTE[JY]: This is probably going to fail...
+  test ("symbolic isFriends 1") {
+    val u =
+      new UserRecord(0
+                  , Constant(0), 0  // name, namep
+                  , Constant(0), 0  // pwd, pwdp
+                  , Constant(0), 0  // username, usernamep
+                  , Constant(0), 0  // email, emailp
+                  , Constant(0), 0  // network, networkp
+                  , List(), 0  // friends, friendsp
+                  , context );
+    val friend1 = pick (x => x === 1)
+    u.addFriend(friend1);
+    // This one is kind of funny...
+    expect(true) { u.isFriends(1) };
+  }
+
+  test ("symbolic isFriends 2") {
+    val u =
+      new UserRecord(0
+                  , Constant(0), 0  // name, namep
+                  , Constant(0), 0  // pwd, pwdp
+                  , Constant(0), 0  // username, usernamep
+                  , Constant(0), 0  // email, emailp
+                  , Constant(0), 0  // network, networkp
+                  , List(), 0  // friends, friendsp
+                  , context );
+    val friend1 = pick (x => x === 1)
+    u.addFriend(1);
+    // TODO: Put a context.
+    expect(true) { concretize(u.isFriends(friend1)) };
+  }
+
+  test ("privacy levels") {
+    val u1 =
+      new UserRecord(0
+                  , Constant(0), 0  // name, namep
+                  , Constant(0), 0  // pwd, pwdp
+                  , Constant(0), 0  // username, usernamep
+                  , Constant(0), 0  // email, emailp
+                  , Constant(0), 0  // network, networkp
+                  , List(), 0  // friends, friendsp
+                  , context );
+    val u2 = 
+      new UserRecord(0
+                  , Constant(0), 0  // name, namep
+                  , Constant(0), 0  // pwd, pwdp
+                  , Constant(0), 0  // username, usernamep
+                  , Constant(0), 0  // email, emailp
+                  , Constant(0), 0  // network, networkp
+                  , List(), 0  // friends, friendsp
+                  , context );
+  }
+
+  test ("equals symbolic self") {
+
+  }
+
+
 }
