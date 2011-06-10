7a63fde57dfea67337eab3551e63c59f9287227b
Kuat Yessenov <kuat@csail.mit.edu>
2011-06-10 18:45:01 -0400

    Upgrade to Scala 2.9.0-1. Downgrade to Z3 2.18 (due to bug in interactive mode -in). Fix deprecations. Change SMT interface to be interactive (and add push/pop).

--- project/build/ScalaSMT.scala
+++ project/build/ScalaSMT.scala
@@ -2,5 +2,5 @@ import sbt._
 
 class ScalaSMT(info: ProjectInfo) extends DefaultProject(info) {
   override def compileOptions = super.compileOptions ++ Seq(Unchecked, Deprecation)
-  val scalatest = "org.scalatest" % "scalatest" % "1.3" % "test"
+  val scalatest = "org.scalatest" % "scalatest_2.9.0" % "1.6.1.RC1" % "test"
 }
