b6b073820bc867c62a49465757600de326bf0435
Jean Yang <jean.yang.writeme@gmail.com>
2011-06-15 11:13:44 -0400

    changed TestJeevesLib to give levels a default

--- src/test/scala/jeeves/TestJeevesLib.scala
+++ src/test/scala/jeeves/TestJeevesLib.scala
@@ -8,51 +8,51 @@ import org.scalatest.Assertions.{expect}
 import scala.collection.immutable.Map
 
 class ExampleJeevesLib extends FunSuite {
-  private val context = pick()
+  private val level = pick(default = 0)
 
   test ("sensitive") {
     val map: SensitiveMap =
       Map((1, 1), (2, 2), (3, 3));
-    val x = JeevesLib.createSensitiveValue(context, map);
-    expect(1) {concretize(context, 1, x)};
-    expect(1) {concretize(context, 1, x)};
-    expect(2) {concretize(context, 2, x)};
-    expect(3) {concretize(context, 3, x)};
+    val x = JeevesLib.createSensitiveValue(level, map);
+    expect(1) {concretize(level, 1, x)};
+    expect(1) {concretize(level, 1, x)};
+    expect(2) {concretize(level, 2, x)};
+    expect(3) {concretize(level, 3, x)};
   }
 
   test ("default") {
     val map: SensitiveMap = 
       Map((JeevesLib.default, 1));
-    val x = JeevesLib.createSensitiveValue(context, map);
-    expect(1) {concretize(context, 0, x)};
+    val x = JeevesLib.createSensitiveValue(level, map);
+    expect(1) {concretize(level, 0, x)};
   }
 
   test("default with other values") {
     val map: SensitiveMap =
       Map((JeevesLib.default, 1), (1, 2));
-    val x = JeevesLib.createSensitiveValue(context, map);
-    expect(1) { concretize(context, 0, x) };
+    val x = JeevesLib.createSensitiveValue(level, map);
+    expect(1) { concretize(level, 0, x) };
 
-    val y = JeevesLib.createSensitiveValue(context, map);
-    expect(2) { concretize(context, 1, y) };
+    val y = JeevesLib.createSensitiveValue(level, map);
+    expect(2) { concretize(level, 1, y) };
   }
 
   test ("mkSensitiveValue") {
     val x =
       JeevesLib.mkSensitiveValue(
           List(2 : BigInt, 1 : BigInt, 0 : BigInt)
-        , context, Constant(42), 1 : BigInt);
-    expect(-1) { concretize(context, 0, x) };
-    expect(42) { concretize(context, 1, x) };
-    expect(42) { concretize(context, 1, x) };
+        , level, Constant(42), 1 : BigInt);
+    expect(-1) { concretize(level, 0, x) };
+    expect(42) { concretize(level, 1, x) };
+    expect(42) { concretize(level, 1, x) };
   }
 
   case class Node(v: Int) extends Atom
   test ("concretizeList non-null") {
     val x = pickAtom(x => x === Node(0));
     val symList = List(x);
-    val context = pickAtom();
-    val cList : List[Node] = concretizeList(context, x, symList);
+    val level = pickAtom();
+    val cList : List[Node] = concretizeList(level, x, symList);
     expect(1) { cList.length };
     expect(0) { cList.head.v };
   }
@@ -60,8 +60,8 @@ class ExampleJeevesLib extends FunSuite {
   test ("concretizeList null") {
     val x = pickAtom(default = NULL);
     val symList = List(x);
-    val context = pickAtom();
-    val cList : List[Node] = concretizeList(context, x, symList);
+    val level = pickAtom();
+    val cList : List[Node] = concretizeList(level, x, symList);
     cList.foreach(x => println(x));
     expect(0) { cList.length };
   }
