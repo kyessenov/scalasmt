e91dec5676d89b9d3ee05c6b25fe87a10a211b20
Jean Yang <jean.yang.writeme@gmail.com>
2011-06-16 14:17:34 -0400

    started writing jconf example

---
+++ src/main/scala/jeeves/jconf/UserLevels.scala
@@ -0,0 +1,10 @@
+package cap.jeeves.jconf
+
+import cap.jeeves.JeevesLib._
+
+object UserLevels {
+  val userDefaultL : LevelTy = 0;
+  val pcL          : LevelTy = 1;
+  val selfL        : LevelTy = 2;
+  val levels = List(userDefaultL, pcL, selfL);
+}
