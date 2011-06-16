e68861bcb67e381cbcd63633d22639fe439d89c3
Jean Yang <jean.yang.writeme@gmail.com>
2011-06-16 16:29:17 -0400

    small updates to jconf

--- src/main/scala/jeeves/jconf/PaperRecord.scala
+++ src/main/scala/jeeves/jconf/PaperRecord.scala
@@ -6,36 +6,48 @@ package cap.jeeves.jconf
  */
 
 import cap.scalasmt._
-import scala.collection.mutable.Map;
-import PaperLevels._
+import scala.collection.immutable.List;
 import UserStatus._
 import cap.jeeves.JeevesLib._
 
-object PaperStage {
-
+object Viewer {
+  val low   : LevelTy = 0
+  val high  : LevelTy = 1
+  val levels = List(low, high)
 }
 
-import PaperStage._
+object PaperStage {
+  val submission    : LevelTy = 0;
+  val review        : LevelTy = 1;
+  val authorReveal  : LevelTy = 2;
+  val publicReveal  : LevelTy = 3;
+}
 
 /* NOTE: We will not be using this with beans for now... */
 class PaperRecord( val id : BigInt
-                , _name : IntExpr, val namep : LevelTy
+                , _name : IntExpr
                 , _authors : List[IntExpr]
-                /* Pass in prviacy level and context variables. */
                 , val context : AtomVar ) extends Atom {
-  val level = pick(default = paperDefaultL)
+  // The name of the paper is always visible to the authors.
+  val nameLevel : IntVar = pick(default = Viewer.low)
+  assume((context~'status > UserStatus.reviewerL) ==> (name === Viewer.high));
+  val name : IntExpr = mkSensitive(nameLevel, _name)
 
-  val name = mkSensitive(_name, namep);
+  // The authors of the paper are visible to the authors themselves and during
+  // the reveal stage.
+  val authorsLevel : IntVar = pick(default = Viewer.low)
+  assume(((context~'status === UserStatus.reviewerL) &&
+    (context~'stage > PaperStage.authorReveal)) ==>
+          (authorsLevel === Viewer.high))
+  val authors : List[IntExpr] = _authors.map(a => mkSensitive(authorsLevel, a))
+
+  private def mkSensitive(levelVar : IntVar, v : IntExpr) : IntExpr = {
+    val isAuthor : Formula = CONTAINS(_authors, context~'name);
+    assume(CONTAINS(Viewer.levels, authorsLevel));
+    assume(isAuthor ==> (levelVar === Viewer.high));
+    mkSensitiveValue(Viewer.levels, levelVar, v, Viewer.high)
+  }
  
-  /* Set initial privacy levels. */
-  assume(level === context~'status)
-  assume(CONTAINS(levels, level))
-
-  private def mkSensitive (v : IntExpr, p : LevelTy) =
-    if (p > PaperLevels.paperDefaultL)
-      mkSensitiveValue(PaperLevels.levels, level, v, p);
-    else v
-
   override def toString = "jcp" + id
   override def hashCode = id.toInt
   override def equals(that: Any) =
