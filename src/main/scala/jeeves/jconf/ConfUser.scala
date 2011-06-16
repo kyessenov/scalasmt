e42b53f18b186e285fdb569b1da904a20f423a22
Jean Yang <jean.yang.writeme@gmail.com>
2011-06-16 17:45:23 -0400

    started writing tests for jconf

--- src/main/scala/jeeves/jconf/ConfUser.scala
+++ src/main/scala/jeeves/jconf/ConfUser.scala
@@ -13,29 +13,34 @@ object UserStatus {
   val authorL  : BigInt = 0
   val reviewerL : BigInt = 1
   val pcL      : BigInt = 2
-  val levels = List (authorL, reviewerL, pcL)
 }
 
 object UserLevels {
   val userDefaultL : LevelTy = 0;
-  val pcL          : LevelTy = 1;
-  val selfL        : LevelTy = 2;
-  val levels = List(userDefaultL, pcL, selfL);
+  val reviewerL    : LevelTy = 1;
+  val pcL          : LevelTy = 2;
+  val selfL        : LevelTy = 3;
+  val levels = List(userDefaultL, reviewerL, pcL, selfL);
 }
 
-class ConfUser( _name : IntExpr, _pwd : IntExpr
-              , val id : BigInt, _email : IntExpr
+/* Conference User */
+class ConfUser( val id : BigInt
+              , _name : IntExpr, _pwd : IntExpr, _email : IntExpr
               , val status : BigInt, val context : AtomVar ) extends Atom {
 
   val level = pick(default = UserLevels.userDefaultL)
 
-  val name = mkSensitive(_name, UserLevels.pcL);
+  val name = mkSensitive(_name, UserLevels.reviewerL);
   val pwd = mkSensitive(_pwd, UserLevels.selfL);
-  val username = mkSensitive(id, UserLevels.selfL);
   val email = mkSensitive(_email, UserLevels.pcL);
  
   /* Set initial privacy levels. */
-  assume((context~'id === id) <==> (level === UserLevels.selfL))
+  assume((context~'id === id) ==> (level === UserLevels.selfL))
+  assume(((!(context~'id === id)) &&
+          (context~'status === UserStatus.reviewerL)) ==>
+          (level === UserLevels.reviewerL))
+  assume(((!(context~'id === id)) && (context~'status === UserStatus.pcL)) ==>
+          (level === UserLevels.pcL))
   assume(CONTAINS(UserLevels.levels, level))
 
   private def mkSensitive (v : IntExpr, p : LevelTy) =
