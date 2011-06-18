f74d4de722726452cd01ee875daf893309049d6f
Kuat Yessenov (kuat-laptop) <Kuat Yessenov (kuat-laptop)@localhost>
2011-06-18 01:13:46 -0400

    Add a test for hour/minute/second choice statement

--- src/main/scala/Choice.scala
+++ src/main/scala/Choice.scala
@@ -6,16 +6,24 @@ package cap.scalasmt
 object ChoiceStmt {
   import SMT._
 
-  def choose(spec: IntVar => Formula): Int = {
+  def choose(spec: IntVar => Formula) = {
     val x = Var.makeInt
     val out = solve(spec(x))
     out(x).toInt;
   }
 
-  def choose(spec: (IntVar, IntVar) => Formula): (Int, Int) = {
+  def choose(spec: (IntVar, IntVar) => Formula) = {
     val x = Var.makeInt
     val y = Var.makeInt
     val out = solve(spec(x, y))
     (out(x).toInt, out(y).toInt)
+  }
+
+  def choose(spec: (IntVar, IntVar, IntVar) => Formula) = {
+    val x = Var.makeInt
+    val y = Var.makeInt
+    val z = Var.makeInt
+    val out = solve(spec(x,y,z))
+    (out(x).toInt, out(y).toInt, out(z).toInt)
   }
 }
