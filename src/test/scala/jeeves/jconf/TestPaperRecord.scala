73c3655024010c77da8560c080d53f224d84eaf9
Jean Yang <jean.yang.writeme@gmail.com>
2011-06-20 11:12:12 -0400

    updated jconf PaperRecord tests

--- src/test/scala/jeeves/jconf/TestPaperRecord.scala
+++ src/test/scala/jeeves/jconf/TestPaperRecord.scala
@@ -11,20 +11,60 @@ import JeevesLib._
 class ExamplePaperRecord extends FunSuite {
   val context = pickAtom();
 
-  /*
-  val author0 =
-    new PaperRecord( 0, Constant(0), Constant(123), Constant(0)
-                , UserStatus.authorL, context);
-  val reviewer0 =
-    new PaperRecord( 1, Constant(1), Constant(456), Constant(0)
-                , UserStatus.reviewerL, context);
-  val pc0 =
-    new PaperRecord( 1, Constant(1), Constant(789), Constant(0)
-                , UserStatus.pcL, context);
+  val paper0 = new PaperRecord(0, 33, List(0, 3), context);
 
+  private def getAuthorCtxt0 (stage : BigInt = PaperStage.submission)
+  : ConfContext =
+    new ConfContext(0, 0, UserStatus.authorL, stage);
+  private def getAuthorCtxt1 (stage : BigInt = PaperStage.submission) 
+  : ConfContext =
+    new ConfContext(3, 3, UserStatus.authorL, stage);
+  private def getAuthorCtxt2 (stage : BigInt = PaperStage.submission)
+  : ConfContext =
+    new ConfContext(4, 4, UserStatus.authorL, stage);
+
+  private def getReviewerCtxt0 (stage : BigInt = PaperStage.submission)
+  : ConfContext =
+    new ConfContext(1, 1, UserStatus.reviewerL, stage);
+  private def getPcCtxt0 (stage : BigInt = PaperStage.submission)
+  : ConfContext =
+    new ConfContext( 2, 2, UserStatus.pcL, stage);
+
+  // Name visibility
   test ("name visibility") {
-    expect (-1) { concretize(context, reviewer0, author0.pwd) };
-    expect (123) { concretize(context, author0, author0.pwd) };
+    expect(33) {
+      concretize(context, getAuthorCtxt0(), paper0.name);
+    }
+    expect(-1) {
+      concretize(context, getAuthorCtxt2(), paper0.name);
+    }
+
+    val viewMap =
+      Map( (PaperStage.submission, 33)
+         , (PaperStage.review, 33)
+         , (PaperStage.authorReveal, 33) );
+    viewMap.foreach {
+      case (stage, r) =>
+        expect (r) {
+          concretize(context, getReviewerCtxt0(stage), paper0.name)
+        };
+        expect (r) {
+          concretize(context, getPcCtxt0(stage), paper0.name);
+        }
+    }
   }
-  */
+
+  // Author visibility
+  test ("author list visibility") {
+    expect (true) {
+      concretize(context, getAuthorCtxt0(0), CONTAINS(paper0.authors, 0))
+    };
+    expect (true) {
+      concretize(context, getAuthorCtxt1(0), CONTAINS(paper0.authors, 0))
+    };
+    expect (false) {
+      concretize(context, getAuthorCtxt2(0), CONTAINS(paper0.authors, 0))
+    };
+
+  }
 }
