631f89a63440bdfb4161c6d3ce1852566816f7ba
Jean Yang <jean.yang.writeme@gmail.com>
2011-06-20 17:42:22 -0400

    started implementing jcal case study

---
+++ src/main/scala/jeeves/jcal/JcalUser.scala
@@ -0,0 +1,44 @@
+package cap.jeeves.jcal
+
+/*
+ * User records for jconf case study.
+ * @author jeanyang
+ */
+
+import cap.scalasmt._
+import scala.collection.mutable.Map;
+import cap.jeeves.JeevesLib._
+
+class JcalUser( val id : BigInt
+             , _pwd : BigInt
+             , val context : AtomVar ) extends Atom {
+  val contextLevel = pick(default = Viewer.low);
+  assume((context~'id === id) ==> (contextLevel === Viewer.high));
+
+  val pwd = {
+    val level = pick(default = Viewer.low);
+    mkSensitive(level, _pwd);
+  }
+
+  private def mkSensitive(levelVar : IntVar, v : IntExpr) : IntExpr = {
+    assume(CONTAINS(Viewer.levels, levelVar));
+    assume((context~'id === id) ==> (levelVar === Viewer.high));
+    mkSensitiveValue(Viewer.levels, levelVar, v, Viewer.high)
+  }
+
+  // TODO: Groups
+  /*
+  // This only operates over logical variables.
+  def addFriend (friendId : BigInt) = {
+    (context~'id === friendId) ==> (contextLevel === Viewer.high);
+  }
+  */
+
+  override def toString = "gu" + id
+  override def hashCode = id.toInt
+  override def equals(that: Any) =
+    if (that.isInstanceOf[JcalUser])
+      (id == that.asInstanceOf[JcalUser].id) 
+    else 
+      false
+}
