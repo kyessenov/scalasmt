feb1dd3e38bf0102e6bf166fa1b48cb11e79df72
Jean Yang <jean.yang.writeme@gmail.com>
2011-07-05 12:23:03 -0400

    updated conference management example

--- src/main/scala/jeeves/jconf/ConfUser.scala
+++ src/main/scala/jeeves/jconf/ConfUser.scala
@@ -16,32 +16,20 @@ object UserStatus {
 }
 
 /* Conference User */
-class ConfUser( val id : BigInt
-              , _name : IntExpr, _email : IntExpr
-              , val status : BigInt ) extends JeevesRecord {
-    private val isSelf : Formula = CONTEXT~'id === id;
+class ConfUser( val name : IntExpr, val status : BigInt ) extends JeevesRecord;
+//    private val isSelf : Formula = CONTEXT~'id === id;
 
+    /*
     val name = {
     val level = mkLevel();
     // Reviewers can see names after authors are revealed 
+    
     policy( level, isSelf);
     policy( level
           , (CONTEXT~'status >= UserStatus.reviewerL) &&
             (CONTEXT~'stage >= PaperStage.decision) );
+    policy( level
+          , (CONTEXT~'stage === PaperStage.public) && isAccepted );
     mkSensitiveInt(level, _name, -1);
   }
-  val email = {
-    val level = mkLevel()
-    policy(level, isSelf);
-    policy(level, CONTEXT~'status === UserStatus.pcL);
-    mkSensitiveInt(level, _email, -1);
-  }
-
-  override def toString = "jcu" + id
-  override def hashCode = id.toInt
-  override def equals(that: Any) =
-    if (that.isInstanceOf[ConfUser])
-      (id == that.asInstanceOf[ConfUser].id) 
-    else 
-      false
-}
+  */
--- src/test/scala/jeeves/jconf/TestConfUser.scala
+++ src/test/scala/jeeves/jconf/TestConfUser.scala
@@ -10,10 +10,10 @@ import scala.collection.immutable.Map
 import JeevesLib._
 
 class ExampleConfUser extends FunSuite {
+  /*
   val author0Info = TestUsers.mkUser(UserStatus.authorL)
   val author0Name = author0Info._1;
-  val author0Email = author0Info._2;
-  val author0 = author0Info._3;
+  val author0 = author0Info._2;
   private def getAuthorCtxt0 (stage : BigInt = PaperStage.submission)
   : ConfContext =
     new ConfContext(author0.id, UserStatus.authorL, stage);
@@ -21,12 +21,12 @@ class ExampleConfUser extends FunSuite {
   : ConfContext = new ConfContext(3, UserStatus.authorL, stage);
 
   val reviewer0Info = TestUsers.mkUser(UserStatus.reviewerL);
-  val reviewer0 = reviewer0Info._3;
+  val reviewer0 = reviewer0Info._2;
   private def getReviewerCtxt0 (stage : BigInt = PaperStage.submission)
   : ConfContext = new ConfContext(reviewer0.id, UserStatus.reviewerL, stage);
 
   val pc0Info = TestUsers.mkUser(UserStatus.pcL);
-  val pc0 = pc0Info._3;
+  val pc0 = pc0Info._2;
   private def getPcCtxt0 (stage : BigInt = PaperStage.submission)
   : ConfContext = new ConfContext( 2, UserStatus.pcL, stage);
 
@@ -51,10 +51,5 @@ class ExampleConfUser extends FunSuite {
     // Other authors should not be able to see the current author ever.
     expect (-1) { concretize(getAuthorCtxt1(), author0.name) };
   }
-  
-  test ("email visibility") {
-    expect (author0Email) {
-      concretize(getAuthorCtxt0(), author0.email)
-    };
-  }
+  */
 }
