fe05a7d46cb799ab3683ed8bd4bce01acca0b75f
Jean Yang <jean.yang.writeme@gmail.com>
2011-07-06 18:22:14 -0400

    added review policies

--- src/main/scala/jeeves/jconf/PaperReview.scala
+++ src/main/scala/jeeves/jconf/PaperReview.scala
@@ -9,30 +9,39 @@ import cap.scalasmt._
 
 import JConfBackend._
 
-/* Conference User */
-case class ReviewBody (body : String, score : Int, confidence : Int)
-  extends JeevesRecord;
+case class Body(val text: String) extends JeevesRecord;
+class ReviewBody ( _body: Body
+                 , val score: BigInt
+                 , _confidence: BigInt
+                 , _isAuthor: Formula)
+  extends JeevesRecord {
+  private val isInternal : Formula = CONTEXT~'STATUS >= UserStatus.reviewerL
+
+  val rlevel = mkLevel();
+  policy(rlevel, isInternal);
+  policy(rlevel, _isAuthor &&
+                (CONTEXT/'stage === Rebuttal || CONTEXT/'stage === Decision));
+
+  val body = mkSensitiveObject(rlevel, _body, NULL)
+  val confidence = mkSensitiveInt(rlevel, _confidence, -1);
+}
+
 class PaperReview( id : Int
-                 , _reviewer : ConfUser
-                 , _review : ReviewBody
+                 , _reviewer: ConfUser
+                 , _rtext: String, _score: Int, _conf: Int
                  , _isAuthor : Formula ) extends JeevesRecord {
   private val Anonymous = new ConfUser(Name("Anonymous"), UserStatus.reviewerL)
-  private val DefaultBody = ReviewBody("", -1, -1)
+  private val DefaultBody = new ReviewBody(null, -1, -1, _isAuthor)
 
   private val isInternal : Formula = CONTEXT~'STATUS >= UserStatus.reviewerL
 
+  // Restrict reviewer to only be seen by internal people.
   val reviewer = {
     val level = mkLevel();
     policy(level, isInternal);
-//    policy(level, CONTEXT/'stage === Decision || CONTEXT/'stage === Rebuttal);
     mkSensitiveObject(level, _reviewer, Anonymous);
   }
 
-  val review = {
-    val level = mkLevel();
-    policy(level, isInternal);
-    policy(level, _isAuthor &&
-                  (CONTEXT/'stage === Rebuttal || CONTEXT/'stage === Decision));
-    mkSensitiveObject(level, _reviewer, DefaultBody);
-  }
+  val review : ReviewBody =
+    new ReviewBody(Body(_rtext), _score, _conf, _isAuthor)
 }
