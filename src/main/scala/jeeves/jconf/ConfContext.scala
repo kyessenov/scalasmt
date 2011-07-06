c35e5aeca7c869489e1dda6b75417c1ddfe6f6f0
Jean Yang <jean.yang.writeme@gmail.com>
2011-07-06 19:09:06 -0400

    small change to UserStatus

--- src/main/scala/jeeves/jconf/ConfContext.scala
+++ src/main/scala/jeeves/jconf/ConfContext.scala
@@ -11,5 +11,5 @@ import scala.collection.mutable.Map;
 import JConfBackend._
 
 case class ConfContext( viewer : ConfUser
-                      , status : BigInt
+                      , status : UserStatus
                       , stage  : PaperStage ) extends JeevesRecord
