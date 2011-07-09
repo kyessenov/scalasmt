85d529dca024830088c3bf08ca86484afc0031b6
Jean Yang <jean.yang.writeme@gmail.com>
2011-07-09 00:00:00 -0400

    some cleanup of conference management system

--- src/main/scala/jeeves/jconf/PaperReview.scala
+++ src/main/scala/jeeves/jconf/PaperReview.scala
@@ -9,39 +9,20 @@ import cap.scalasmt._
 
 import JConfBackend._
 
-case class Body(val text: String) extends JeevesRecord;
-class ReviewBody ( _body: Body
-                 , val score: BigInt
-                 , _confidence: BigInt
-                 , _isAuthor: Formula)
-  extends JeevesRecord {
-  private val isInternal : Formula = CONTEXT.status >= UserStatus.reviewerL
-
-  val rlevel = mkLevel();
-  // policy(rlevel, isInternal);
-  // policy(rlevel, _isAuthor &&
-  //              (CONTEXT.stage === Rebuttal || CONTEXT.stage === Decision));
-
-  val body = mkSensitiveObject(rlevel, _body, NULL)
-  val confidence = mkSensitiveInt(rlevel, _confidence, -1);
-}
-
 class PaperReview( id : Int
                  , _reviewer: ConfUser
-                 , _rtext: String, _score: Int, _conf: Int
-                 , _isAuthor : Formula ) extends JeevesRecord {
-  private val Anonymous = new ConfUser(Name("Anonymous"), UserStatus.reviewerL)
-  private val DefaultBody = new ReviewBody(null, -1, -1, _isAuthor)
+                 , val body: String, val score: Int) extends JeevesRecord {
+  private val Anonymous = new ConfUser(Name("Anonymous"), ReviewerStatus)
 
-  private val isInternal : Formula = CONTEXT.status >= UserStatus.reviewerL
+  private val isInternal : Formula =
+    (CONTEXT.viewer.status === ReviewerStatus) || 
+    (CONTEXT.viewer.status === PCStatus)
 
   // Restrict reviewer to only be seen by internal people.
   val reviewer = {
     val level = mkLevel();
-    // policy(level, isInternal);
+    policy(level, isInternal, HIGH);
+    policy(level, !isInternal, LOW);
     mkSensitiveObject(level, _reviewer, Anonymous);
   }
-
-  val review : ReviewBody =
-    new ReviewBody(Body(_rtext), _score, _conf, _isAuthor)
 }
