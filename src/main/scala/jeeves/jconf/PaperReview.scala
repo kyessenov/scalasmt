761a9bbe2aafa0192c2c5820bdefdfd14b25b175
Jean Yang <jean.yang.writeme@gmail.com>
2011-07-08 22:13:48 -0400

    updates to conference management example

--- src/main/scala/jeeves/jconf/PaperReview.scala
+++ src/main/scala/jeeves/jconf/PaperReview.scala
@@ -10,26 +10,24 @@ import cap.scalasmt._
 import JConfBackend._
 
 case class Body(val text: String) extends JeevesRecord;
-case class ReviewBody (
-  val body: Body, val score: BigInt, val confidence: BigInt)
+case class ReviewBody (val body: Body, val score: BigInt)
   extends JeevesRecord;
 
 class PaperReview( id : Int
                  , _reviewer: ConfUser
-                 , _rtext: String, _score: Int, _conf: Int
+                 , _rtext: String, _score: Int
                  , _isAuthor : Formula ) extends JeevesRecord {
   private val Anonymous = new ConfUser(Name("Anonymous"), ReviewerStatus)
-  private val DefaultBody = ReviewBody(null, -1, -1)
-
-  private val isInternal : Formula =
-    (CONTEXT/'status === ReviewerStatus) || (CONTEXT/'status === PCStatus)
+  private val DefaultBody = ReviewBody(null, -1)
 
   // Restrict reviewer to only be seen by internal people.
   val reviewer = {
     val level = mkLevel();
-    policy(level, isInternal);
+    policy( level
+          , (CONTEXT/'status === ReviewerStatus) ||
+            (CONTEXT/'status === PCStatus));
     mkSensitiveObject(level, _reviewer, Anonymous);
   }
 
-  val review : ReviewBody = ReviewBody(Body(_rtext), _score, _conf)
+  val review : ReviewBody = ReviewBody(Body(_rtext), _score)
 }
