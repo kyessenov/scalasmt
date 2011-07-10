8a6b92d1dfc45da941a31eb75259d304799ef533
Kuat Yessenov <kuat@csail.mit.edu>
2011-07-09 20:44:48 -0400

    Fix failed merge

--- src/main/scala/jeeves/jconf/PaperRecord.scala
+++ src/main/scala/jeeves/jconf/PaperRecord.scala
@@ -20,7 +20,7 @@ object Public extends PaperStage
 
 sealed trait PaperTag extends JeevesRecord
 object NeedsReview extends PaperTag
-case class ReviewedBy (name : Symbolic) extends PaperTag
+case class ReviewedBy (reviewer: ConfUser) extends PaperTag
 object Accepted extends PaperTag
 
 case class Title (name : String) extends JeevesRecord
@@ -38,11 +38,13 @@ class PaperRecord( val id : Int
     (CONTEXT.viewer.role === PCStatus)
 
   // The name of the paper is always visible to the authors.
-  val name : Symbolic = {
+  def updateName(_name: Title): Symbolic = {
     val level = mkLevel ();
     policy (level, ! (isAuthor || isInternal || isPublic(getTags())), LOW);
     mkSensitive[Title](level, _name, Title(""))
   }
+
+  var name = updateName(_name);
 
   val authors : List[Symbolic] = {
     val level = mkLevel ();
@@ -93,8 +95,7 @@ class PaperRecord( val id : Int
     reviewIds = reviewIds + 1;
     id
   }
-  private def dummyReview = new PaperReview(-1, null, "", -1)
-  val reviews : Map[Int, Symbolic] = Map[Int, Symbolic]()
+  var reviews: List[Symbolic] = Nil;
   def addReview (reviewer: ConfUser, rtext: String, score: Int)
   : Symbolic = {
     val reviewId = getReviewId ();
@@ -102,14 +103,15 @@ class PaperRecord( val id : Int
       val level = mkLevel();
       val s = new PaperReview(reviewId, reviewer, rtext, score);
       val canSee =
-        isAuthor &&
-          ((CONTEXT.stage === Rebuttal) || (CONTEXT.stage === Decision));
+        isInternal || 
+        (isAuthor &&
+          ((CONTEXT.stage === Rebuttal) || (CONTEXT.stage === Decision)));
       policy(level, canSee, HIGH);
       policy(level, ! canSee, LOW);
-      mkSensitive[PaperReview](level, s, dummyReview)
+      mkSensitive[PaperReview](level, s, NULL)
     }
-    reviews + (reviewId -> r);
-    addTag (ReviewedBy(r.reviewer))
+    reviews = r :: reviews;
+    addTag (ReviewedBy(reviewer))
     r
   }
 }
