27db408cf420e18ef40124c358b739b365e89e4a
Kuat Yessenov <kuat@csail.mit.edu>
2011-07-11 14:48:51 -0400

    Always hide high values inside setters.

--- src/main/scala/jeeves/socialnet/UserRecord.scala
+++ src/main/scala/jeeves/socialnet/UserRecord.scala
@@ -30,13 +30,18 @@ class UserRecord(
   friendsL: UserLevel) 
 extends JeevesRecord {
   private var friends: Set[UserRecord] = Set()
-  private var X: BigInt = 0;
-  private var Y: BigInt = 0;
+  private var X: IntExpr = 0
+  private var Y: IntExpr = 0;
 
   /** Mutators */
   def add(u: UserRecord) {friends = friends + u}
   def remove(u: UserRecord) {friends = friends - u}
-  def setLocation(x: BigInt, y: BigInt) {this.X = x; this.Y = y;}
+  def setLocation(x: BigInt, y: BigInt) {
+    val l = mkLevel();
+    policy(l, DISTANCE(CONTEXT, this) >= 10, LOW);
+    this.X = mkSensitiveInt(l, x, 1000);
+    this.Y = mkSensitiveInt(l, y, 1000);
+  }
 
   /** Observers */
   val name = mkSensitive[Name](level(nameL), nameV)
@@ -47,11 +52,7 @@ extends JeevesRecord {
     friends.map(mkSensitive[UserRecord](l, _))
   }
   def isFriends(u: UserRecord) = getFriends.has(u)
-  def location() = {
-    val l = mkLevel();
-    policy(l, DISTANCE(CONTEXT, this) >= 10, LOW)
-    (mkSensitiveInt(l, X), mkSensitiveInt(l, Y))
-  }
+  def location() = (X, Y);
 
   /** Helpers */
   private def level(ul: UserLevel) = {
@@ -60,8 +61,7 @@ extends JeevesRecord {
     ul match {
       case Anyone => 
       case Self => policy(l, ! me, LOW)
-      case Friends => 
-        policy(l, ! (me || friends.has(CONTEXT)),  LOW);
+      case Friends => policy(l, ! (me || friends.has(CONTEXT)),  LOW);
     }
     l
   }
