04b271e5dd8dfb81b95f55f72fc02379adb92078
jean yang writeme <jean.yang.writeme@gmail.com>
2012-01-10 16:21:31 -0500

    removed jconf

---
+++ src/main/scala/jeeves/JeevesLib.scala
@@ -0,0 +1,68 @@
+package cap.jeeves
+
+/*
+ * A library for using ScalaSMT for privacy, using symbolic varaibles to
+ * represent sensitive values.
+ * @author jeanyang, kuat
+ */
+
+import cap.scalasmt._
+import scala.collection.immutable.Map;
+import scala.collection.mutable.{Map => MMap};
+import scala.collection.mutable.HashMap;
+
+trait JeevesLib extends Sceeves {
+  trait JeevesRecord extends Atom {
+    register(this)
+  }
+  type LevelVar = BoolVar;
+  type Symbolic = ObjectExpr[Atom];
+  
+  sealed trait Level
+  object HIGH extends Level
+  object LOW extends Level
+  implicit def level2sym(l: Level): Formula = l match {
+    case HIGH => true
+    case LOW => false
+  }
+
+  val CONTEXT: Symbolic = pickObject[JeevesRecord];
+  
+  private var POLICIES: List[(LevelVar, Level, () => Formula)] = Nil
+
+  def mkLevel(): LevelVar = pickBool(_ => true, HIGH)
+
+  def mkSensitiveInt(lvar: LevelVar, high: IntExpr, low: IntExpr = -1): IntExpr = 
+    lvar ? high ! low
+  
+  def mkSensitive(lvar: LevelVar, high: Symbolic, low: Symbolic = NULL): Symbolic = 
+    lvar ? high ! low
+   
+  def policy(lvar: LevelVar, f: => Formula, value: Level) = 
+    POLICIES = (lvar, value, f _) :: POLICIES
+  
+  override def assume(f: Formula) = super.assume(Partial.eval(f)(EmptyEnv))
+
+  def concretize[T](ctx: Symbolic, e: Expr[T]) = {
+    Debug.debug(" *** # POLICIES: " + POLICIES.size)
+    val context = (CONTEXT === ctx) && 
+      AND(POLICIES.map{
+        case (lvar, level, f) => f() ==> (lvar === level)
+      })
+    super.concretize(context, e);
+  }
+
+  /**
+   * Collections of symbolic values.
+   */ 
+
+  def concretize[T](ctx: Symbolic, e: (Expr[T], Expr[T])): (T, T) = 
+    (concretize(ctx, e._1), concretize(ctx, e._2))
+
+  def concretize[T >: Null <: JeevesRecord](ctx: Symbolic, lst: Traversable[Symbolic]): List[T] = 
+    for (o <- lst.toList;
+      t = concretize(ctx, o).asInstanceOf[T];
+      if (t != null))
+      yield t;
+}
+
