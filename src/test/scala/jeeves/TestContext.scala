2f7d9945f543c9b2508a5f26b5f5c9f288a9d711
jean yang writeme <jean.yang.writeme@gmail.com>
2012-02-22 10:40:40 -0500

    TestContext cases pass now

--- src/test/scala/jeeves/TestContext.scala
+++ src/test/scala/jeeves/TestContext.scala
@@ -7,23 +7,37 @@ import cap.jeeves._
 
 class ExampleContext extends FunSuite with JeevesLib {
 
-  case class Dummy(ID: BigInt) extends Atom
+  case class Dummy(ID: BigInt) extends JeevesRecord
 
   def eval[T](expr: Expr[T]) = expr.eval
 
   def mkElt(x: Dummy): Symbolic = {
     val l = mkLevel();
-    policy(l, !(CONTEXT === Dummy(1)), LOW);
+    policy(l, !(CONTEXT.ID === IntVal(1)), LOW);
     mkSensitive(l, x, Dummy(0))
   }
+
+  val x: Dummy = Dummy(1);
+  val x_s: Symbolic = mkElt(x);
+  
   val c = (1 to 3).toList.map(Dummy(_))
   val s = c.map(mkElt)
 
+
+  test ("high confidentialy context") {
+    expect (x) { concretize(x, x_s) }
+  }
+
   test ("low confidentiality context") {
+    expect (Dummy(0)) { concretize(NULL, x_s) }
+  }
+
+  /* Lists. */
+  test ("low confidentiality context - list") {
     expect(true) { concretize(NULL, s.has(Dummy(0))) }
   }
 
-  test ("high confidentiality context") {
-    expect(true) { concretize(Dummy(1), s.has(Dummy(1))) }
+  test ("high confidentiality context - list") {
+    expect(true) { concretize[Boolean](Dummy(1), s.has(Dummy(1))) }
   }
 }
